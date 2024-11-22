a021a45ca7085b39374df285eb15f72a783ba777
cagdaskaya <cagdas.kaya@sky.uk>
2024-11-22 21:50:52 +0000

    new dante table approach

--- app.py
+++ app.py
@@ -10,34 +10,34 @@ app = Flask(__name__)
 # app.jinja_env.trim_blocks = False
 
 VDC_NETS = {
-    'cdpvdc-bri': '10.234.14.0/24',
-    'cdpvdc-slo': '10.234.78.0/24',
-    'mia-prd-bri': '10.234.33.176/29',
-    'mia-prd-slo': '10.234.99.176/29',
-    'mia-stg-bri': '10.234.33.184/29',
-    'mia-stg-slo': '10.234.99.184/29',
-    'skynet-prd-bri': '10.234.32.128/25',
-    'skynet-prd-slo': '10.234.97.128/25',
-    'skynet-stg-bri': '10.234.32.0/25',
-    'skynet-stg-slo': '10.234.97.0/25',
-    'tsf-prd-bri': '10.234.31.32/27',
-    'tsf-prd-slo': '10.234.104.32/27',
-    'tsf-stg-bri': '10.234.31.64/27',
-    'tsf-stg-slo': '10.234.104.64/27',
-    'utils-prd-bri-1': '10.234.31.128/29',
-    'utils-prd-slo-1': '10.234.104.128/29',
-    'utils-stg-bri-1': '10.234.31.136/29',
-    'utils-stg-slo-1': '10.234.104.136/29',
-    'utils-prd-bri-2': '10.245.225.224/28',
-    'utils-prd-slo-2': '10.245.229.224/28',
-    'utils-stg-bri-2': '10.245.225.240/28',
-    'utils-stg-slo-2': '10.245.229.240/28',
-    'vodcm-prd-bri': '10.234.31.96/28',
-    'vodcm-prd-slo': '10.234.104.96/28',
-    'vodcm-stg-bri': '10.234.31.112/28',
-    'vodcm-stg-slo': '10.234.104.112/28',
-    'vso-prd-slo': '10.234.79.0/28',
-    'vso-stg-slo': '10.234.99.0/25'
+    '10.234.14.0/24': 'cdpvdc-bri',
+    '10.234.78.0/24': 'cdpvdc-slo',
+    '10.234.33.176/29': 'mia-prd-bri',
+    '10.234.99.176/29': 'mia-prd-slo',
+    '10.234.33.184/29': 'mia-stg-bri',
+    '10.234.99.184/29': 'mia-stg-slo',
+    '10.234.32.128/25': 'skynet-prd-bri',
+    '10.234.97.128/25': 'skynet-prd-slo',
+    '10.234.32.0/25': 'skynet-stg-bri',
+    '10.234.97.0/25': 'skynet-stg-slo',
+    '10.234.31.32/27': 'tsf-prd-bri',
+    '10.234.104.32/27': 'tsf-prd-slo',
+    '10.234.31.64/27': 'tsf-stg-bri',
+    '10.234.104.64/27': 'tsf-stg-slo',
+    '10.234.31.128/29': 'utils-prd-bri',
+    '10.234.104.128/29': 'utils-prd-slo',
+    '10.234.31.136/29': 'utils-stg-bri',
+    '10.234.104.136/29': 'utils-stg-slo',
+    '10.245.225.224/28': 'utils-prd-bri',
+    '10.245.229.224/28': 'utils-prd-slo',
+    '10.245.225.240/28': 'utils-stg-bri',
+    '10.245.229.240/28': 'utils-stg-slo',
+    '10.234.31.96/28': 'vodcm-prd-bri',
+    '10.234.104.96/28': 'vodcm-prd-slo',
+    '10.234.31.112/28': 'vodcm-stg-bri',
+    '10.234.104.112/28': 'vodcm-stg-slo',
+    '10.234.79.0/28': 'vso-prd-slo',
+    '10.234.99.0/25': 'vso-stg-slo'
 }
 
 
@@ -117,58 +117,6 @@ def squid_rule_check(conf):
     return squid_rules
 
 
-def dante_rule_check(conf):
-    client_rules = []
-    socks_rules = []
-    for x, i in enumerate(conf):
-        if (i.count('pass {') or i.count('block {')) and not i.startswith('#'):
-            src_net = conf[x+1].split('from: ')[1].split()[0]
-            src_bg = [
-                bg for bg, net in VDC_NETS.items() if ip_network(
-                    src_net, strict=False).subnet_of(ip_network(net))
-            ]
-            dst_net = conf[x+1].split('to: ')[1].split()[0]
-            if i.startswith('socks'):
-                socks_rules.append(dict())
-                s = len(socks_rules) - 1
-                if i.count('pass'):
-                    socks_rules[s]['action'] = 'ALLOW'
-                else:
-                    socks_rules[s]['action'] = 'DENY'
-                if src_bg:
-                    socks_rules[s]['SRC'] = f'{src_net} - {src_bg[0]}'
-                else:
-                    socks_rules[s]['SRC'] = f'{src_net}'
-                socks_rules[s]['DST'] = dst_net
-                if conf[x+1].count('port: '):
-                    socks_rules[s]['DST_port'] = (
-                        conf[x+1].split('port: ')[1].split()[0]
-                    )
-                else:
-                    socks_rules[s]['DST_port'] = 'ANY'
-                if conf[x+2].count('command: '):
-                    socks_rules[s]['command'] = conf[x+2].split('command: ')[1]
-            if i.startswith('client'):
-                client_rules.append(dict())
-                c = len(client_rules) - 1
-                if i.count('pass'):
-                    client_rules[c]['action'] = 'ALLOW'
-                else:
-                    client_rules[c]['action'] = 'DENY'
-                if src_bg:
-                    client_rules[c]['SRC'] = f'{src_net} - {src_bg[0]}'
-                else:
-                    client_rules[c]['SRC'] = f'{src_net}'
-                client_rules[c]['DST'] = dst_net
-                if conf[x+1].count('port: '):
-                    client_rules[c]['DST_port'] = (
-                        conf[x+1].split('port: ')[1].split()[0]
-                    )
-                else:
-                    client_rules[c]['DST_port'] = 'ANY'
-    return (socks_rules, client_rules)
-
-
 def parse_dante_config(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
@@ -193,7 +141,6 @@ def parse_dante_config(file_path):
 
     # Step 2: Parse serialized rules
     for rule in serialized_rules:
-        # rule_type = "client" if "client" in rule else "socks"
         rule_type = "client" if rule.startswith("client") else (
             "socks" if rule.startswith("socks") else None
         )
@@ -219,16 +166,30 @@ def parse_dante_config(file_path):
             from_list = from_matches.group(1).split() if from_matches else []
             to_list = to_matches.group(1).split() if to_matches else []
             port = port_matches.group(1).split() if port_matches else ["ANY"]
-            command = command_matches.group(1).split() if command_matches else None
-            print(f'port: {port}')
-            print(f'command: {command}')
-            print(f'from: {from_list}')
+            command = (command_matches.group(1).split()
+                       if command_matches else None)
+
+            enriched_from_list = []
+            for src in from_list:
+                try:
+                    src_network = ip_network(src, strict=False)
+                    bg = next(
+                        (bg_name for net, bg_name in VDC_NETS.items()
+                         if src_network.subnet_of(ip_network(net))),
+                        None
+                    )
+                    if bg:
+                        enriched_from_list.append(f"{src} - {bg}")
+                    else:
+                        enriched_from_list.append(src)
+                except ValueError:
+                    enriched_from_list.append(src)
 
             # Create a dictionary for each rule
             parsed_rules.append({
                 "type": rule_type,
                 "action": action,
-                "SRC": from_list,
+                "SRC": enriched_from_list,
                 "DST": to_list,
                 "DST_port": port,
                 "command": command,
@@ -260,24 +221,22 @@ def squid_stg_func():
     )
 
 
-@app.route('/dante/prd')
+@app.route('/dante-prd')
 def dante_prd_func():
-    dante_prd_conf = update_contents('sockd-prod.conf.j2')
-    (socks_prd_rules, client_prd_rules) = dante_rule_check(dante_prd_conf)
+    # Parse the configuration file
+    update_contents('sockd-prod.conf.j2')
+    parsed_rules = parse_dante_config('sockd-prod.conf.j2')
+
+    # Separate SOCKS and client rules
+    socks_rules = [rule for rule in parsed_rules if rule["type"] == "socks"]
+    client_rules = [rule for rule in parsed_rules if rule["type"] == "client"]
+
     return render_template(
-        'dante_prd.html', socks_prd_rules=socks_prd_rules,
-        client_prd_rules=client_prd_rules, enumerate=enumerate
+        'dante_prd.html',
+        socks_prd_rules=socks_rules,
+        client_prd_rules=client_rules,
+        enumerate=enumerate
     )
-
-
-# @app.route('/dante/stg')
-# def dante_stg_func():
-#     dante_stg_conf = update_contents('sockd-stage.conf.j2')
-#     (socks_stg_rules, client_stg_rules) = dante_rule_check(dante_stg_conf)
-#     return render_template(
-#         'dante_stg.html', socks_stg_rules=socks_stg_rules,
-#         client_stg_rules=client_stg_rules, enumerate=enumerate
-#     )
 
 
 @app.route('/dante-stg')
